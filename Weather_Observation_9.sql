bc2e663d51b79776c7e3ec5505577b92faaf27e2
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-26 21:44:27 +0530

    Update Weather_Observation_9.sql

--- Weather_Observation_9.sql
+++ Weather_Observation_9.sql
@@ -1,5 +1,28 @@
+-- Question : Weather Observation 10
 --Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY NOT LIKE '[aeiou]%'
+
+-- Question : Weather Observation 10
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE '%[aeiou]'
+
+-- Question :  Weather Observation 11
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE '[aeiou]%' OR CITY NOT LIKE '%[aeiou]';
+
+-- Question :  Weather Observation 12
+--Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE '[aeiou]%' AND CITY NOT LIKE '%[aeiou]';
+
