128eb7da206531e50ca332d847c280b97ac0a905
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-27 11:56:26 +0530

    Update Types_of_triangle.sql

--- Types_of_triangle.sql
+++ Types_of_triangle.sql
@@ -12,9 +12,9 @@ Given a table TRIANGLES that holds data for three fields namely A, B, C.
  
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
 
-Equilateral   : It's a triangle with  sides of equal length.
-Isosceles     : It's a triangle with  sides of equal length.
-Scalene       : It's a triangle with  sides of differing lengths.
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
 Not A Triangle: The given values of A, B, and C don't form a triangle.
 */
 
