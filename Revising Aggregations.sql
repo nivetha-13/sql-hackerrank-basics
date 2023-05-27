2029ab5a156ff3954f2d519d4d1da1c3af538ec5
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-27 11:37:54 +0530

    Update and rename Revising Aggregations - The Count Function.sql to Revising Aggregations.sql

--- Revising Aggregations.sql
+++ Revising Aggregations.sql
@@ -8,3 +8,8 @@ WHERE POPULATION > 100000;
 SELECT SUM(POPULATION)
 FROM CITY
 WHERE DISTRICT = 'California';
+
+--Query the average population of all cities in CITY where District is California.
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
