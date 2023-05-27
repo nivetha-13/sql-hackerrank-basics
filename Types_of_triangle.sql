8f81f41a9bb9a27d05a04428d6d3caf0e05aa56a
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-27 11:27:34 +0530

    Update Types_of_triangle.sql

--- Types_of_triangle.sql
+++ Types_of_triangle.sql
@@ -19,3 +19,10 @@ Not A Triangle: The given values of A, B, and C don't form a triangle.
 */
 
 --Solution
+SELECT CASE
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+WHEN A = B AND B = C THEN 'Equilateral'
+WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM TRIANGLES;
