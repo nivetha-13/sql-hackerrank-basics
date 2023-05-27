dbcee91b5bea0f0ce0adf4035f8549d3cc51d3c4
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-27 11:26:31 +0530

    Update and rename SQLQuery1.sql to Types_of_triangle.sql

---
+++ Types_of_triangle.sql
@@ -0,0 +1,21 @@
+--Problem Statement
+
+/*
+Given a table TRIANGLES that holds data for three fields namely A, B, C.
++-------------+------------+
+| Column      |   Type     |
++-------------+------------+
+| A           | INTEGER    |
+| B           | INTEGER    |
+| C           | INTEGER    |
++-------------+------------+
+ 
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral   : It's a triangle with  sides of equal length.
+Isosceles     : It's a triangle with  sides of equal length.
+Scalene       : It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+
+--Solution
