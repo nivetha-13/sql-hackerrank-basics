59b3b002ce77b62c3ef371df736c9b455e8ff610
Nivetha Murali <94458999+nivetha-13@users.noreply.github.com>
2023-05-29 21:25:22 +0530

    Update Aggregations.sql

--- Aggregations.sql
+++ Aggregations.sql
@@ -84,3 +84,26 @@ WHERE LAT_N < 137.2345;
 SELECT ROUND(MAX(LAT_N),4)
 FROM STATION
 WHERE LAT_N < 137.2345;
+
+--Problem Statement --- Weather Observation 15
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Write a query to find the corresponding Western Longitude value for the greatest value of the Northern Latitudes less than 137.2345 up to 4 decimal places.
+*/
+
+--Solution
+
+SELECT CAST(LONG_W AS DECIMAL(10,4))
+FROM STATION
+WHERE LAT_N = (select MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
